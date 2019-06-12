3b41613dd46be5b60cc8f5e0accf6aa2101e03cc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-12 15:38:30 -0400

    Update CashIDTest.php

--- tests/CashID/CashIDTest.php
+++ tests/CashID/CashIDTest.php
@@ -33,16 +33,58 @@ class CashIDTest extends \PHPUnit\Framework\TestCase
         ];
         $requestURI = $this->cashid->createRequest("login", "15366-4133-6141-9638", $metadata);
         $this->assertEquals("cashid:demo.cashid.info/api/parse.php?a=login&d=15366-4133-6141-9638&r=c3&o=p4&x=", substr($requestURI, 0, -9));
-        $this->assertRegExp('/^\d{9}$/', substr($requestURI, -9));
+        $nonce = substr($requestURI, -9);
+        $this->assertRegExp('/^\d{9}$/', $nonce);
         $request_parts = $this->cashid->parseRequest($requestURI);
 
         $expected_array = [
             "parameters" => [
                 "action" => "login",
                 "data" => "15366-4133-6141-9638",
-                "optional" => ["position" => ["streetname"]],
-                "required" => ["contact" => ["social"]],
+                "optional" => [
+                    "position" => "",
+                    'identification' => ''
+                    'name' => ''
+                    'family' => ''
+                    'nickname' => ''
+                    'age' => ''
+                    'gender' => ''
+                    'birthdate' => ''
+                    'picture' => ''
+                    'national' => ''
+                    'country' => ''
+                    'state' => ''
+                    'city' => ''
+                    'streetname' => '4',
+                ],
+                "required" => [
+                    'contact' => ''
+                    'identification' => ''
+                    'name' => ''
+                    'family' => ''
+                    'nickname' => ''
+                    'age' => ''
+                    'gender' => ''
+                    'birthdate' => ''
+                    'picture' => ''
+                    'national' => ''
+                    'position' => ''
+                    'country' => ''
+                    'state' => ''
+                    'city' => ''
+                    'streetname' => ''
+                    'streetnumber' => ''
+                    'residence' => ''
+                    'coordinate' => ''
+                    'email' => ''
+                    'instant' => ''
+                    'social' => '3'
+                ],
+                'nonce' => $nonce,
             ],
+            'scheme' => 'cashid:',
+            'domain' => 'demo.cashid.info',
+            'path' => '/api/parse.php',
         ];
         $this->assertEquals($expected_array, $request_parts);
     }
