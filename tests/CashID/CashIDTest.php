bf3ef6ec73ee5d49f41974cf71a6a3eac543427c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-12 09:59:09 -0400

    Create CashIDTest.php

---
+++ tests/CashID/CashIDTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace CashID\Tests\CashID;
+
+use CashID\CashID;
+
+class CashIDTest extends \PHPUnit\Framework\TestCase
+{
+    public function setUp()
+    {
+        $this->cashid = new CashID();
+    }
+
+    /**
+     * @testCase constructor
+     */
+    public function testConstructor()
+    {
+        $this->assertInstanceOf(CashID::class, $this->cashid);
+    }
+}
