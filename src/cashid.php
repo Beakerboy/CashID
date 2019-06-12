8d569b61c984d8c71ee1d5fe98bdd9f818a8c4b3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-12 08:06:00 -0400

    Ws

--- src/cashid.php
+++ src/cashid.php
@@ -514,83 +514,69 @@ class JSONRPC
         $rpc_url = RPC_SCHEME . RPC_USERNAME . ":" . RPC_PASSWORD . "@" . RPC_HOST . ":" . RPC_PORT;
 
         // Set up an RPC request.
-        $rpc_request = 
-			[
-				'jsonrpc' => '2.0',
-				'id' => self::$rpc_request_id++,
-				'method' => $rpc_function,
-				'params' => $rpc_arguments
-			];
+        $rpc_request = [
+            'jsonrpc' => '2.0',
+            'id' => self::$rpc_request_id++,
+            'method' => $rpc_function,
+            'params' => $rpc_arguments,
+        ];
 
-			// Set up a stream request.
-			$stream_request = 
-			[
-				'http' =>
-				[
-					'method' => 'POST',
-					'header' => 'Content-type: application/json',
-					'content' => json_encode($rpc_request),
-					'ignore_errors' => true
-				]
-			];
+        // Set up a stream request.
+        $stream_request = [
+            'http' => [
+                'method' => 'POST',
+                'header' => 'Content-type: application/json',
+                'content' => json_encode($rpc_request),
+                'ignore_errors' => true,
+            ],
+        ];
 
-			// Create a connection with the RCP host.
-			$stream_context = stream_context_create($stream_request);
+        // Create a connection with the RCP host.
+        $stream_context = stream_context_create($stream_request);
 
-			// Send the request and store the full response.
-			$rpc_response = file_get_contents($rpc_url, false, $stream_context);
+        // Send the request and store the full response.
+        $rpc_response = file_get_contents($rpc_url, false, $stream_context);
 
-			// Validate if the request was completed
-			if($rpc_response === false)
-			{
-				// Store error description
-				self::$rpc_error = 'Unable to complete RPC request.';
+        // Validate if the request was completed
+        if($rpc_response === false) {
+            // Store error description
+            self::$rpc_error = 'Unable to complete RPC request.';
 
-				// Return NULL to indicate that an error was encountered.
-				return NULL;
-			}
-			else
-			{
-				// Attempt to decode the RCP response
-				$object = @json_decode($rpc_response, true);
+            // Return NULL to indicate that an error was encountered.
+            return NULL;
+        } else {
+            // Attempt to decode the RCP response
+            $object = @json_decode($rpc_response, true);
 
-				// Validate if the RPC response is a valid JSON string.
-				if($object === NULL)
-				{
-					// Store error description
-					self::$rpc_error = 'Reply from RPC host was not a valid JSON string.';
+            // Validate if the RPC response is a valid JSON string.
+            if($object === NULL) {
+                // Store error description
+                self::$rpc_error = 'Reply from RPC host was not a valid JSON string.';
 
-					// Return NULL to indicate that an error was encountered.
-					return NULL;
-				}
-				else
-				{
-					// Check if the response contains any errors.
-					if($object['error'])
-					{
-						// Store the response error message.
-						self::$rpc_error = $object['error']['message'];
+                // Return NULL to indicate that an error was encountered.
+                return NULL;
+            } else {
+                // Check if the response contains any errors.
+                if($object['error']) {
+                    // Store the response error message.
+                    self::$rpc_error = $object['error']['message'];
 
-						// Return NULL to indicate that an error was encountered.
-						return NULL;
-					}
-					else
-					{
-						// Clear any previous error descriptions.
-						self::$rpc_error = '';
+                    // Return NULL to indicate that an error was encountered.
+                    return NULL;
+                } else {
+                    // Clear any previous error descriptions.
+                    self::$rpc_error = '';
 
-						// Return the RPC response data.
-						return $object['result'];
-					}
-				}
-			}
-		}
-	}
+                    // Return the RPC response data.
+                    return $object['result'];
+                }
+            }
+        }
+    }
+}
 
-	// Create an internal exception type which lets us catch our own exceptions 
-	// while still passing on system exceptions that we didn't handle.
-	class InternalException extends \Exception {}
-
-	// Create an instance of the class to retain compatibility with previous versions.
-	$cashid = new CashID();
-?>
+// Create an internal exception type which lets us catch our own exceptions 
+// while still passing on system exceptions that we didn't handle.
+class InternalException extends \Exception
+{
+}
