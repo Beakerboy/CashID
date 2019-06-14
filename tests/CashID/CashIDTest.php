f4e8ddbae0b792a6f6058babc05eb8d5efbdfdec
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-14 08:44:12 -0400

    Update CashIDTest.php

--- tests/CashID/CashIDTest.php
+++ tests/CashID/CashIDTest.php
@@ -116,35 +116,35 @@ class CashIDTest extends \PHPUnit\Framework\TestCase
      */
     public function testInvalidRequest($JSON_string)
     {
-        $this->assertFalse($this->cashid->validateRequest($JSON));
+        $this->assertFalse($this->cashid->validateRequest($JSON_string));
     }
 
     public function dataProviderForInvalidRequest()
     {
         return [
             [  // Not a JSON String
-                "Not JSON",
+                'Not JSON',
             ],
             [  // Missing request
-                "{'address': 'qqagsast3fq0g43wnrnweefjsk28pmyvwg7t0jqgg4'}",
+                '{"address": "qqagsast3fq0g43wnrnweefjsk28pmyvwg7t0jqgg4"}',
             ],
             [  // Missing address
-               "{
-                    'request': 'cashid:bitcoin.com/api/cashid?a=register&d=newsletter&r=i12l1c1&o=i458l3&x=95261230581'
-                }"
+               '{
+                    "request": "cashid:bitcoin.com/api/cashid?a=register&d=newsletter&r=i12l1c1&o=i458l3&x=95261230581"
+                }'
             ],
             [  // Missing Signature
-                "{
-                    'request': 'cashid:bitcoin.com/api/cashid?a=register&d=newsletter&r=i12l1c1&o=i458l3&x=95261230581',
-                    'address': 'qqagsast3fq0g43wnrnweefjsk28pmyvwg7t0jqgg4'
-                }",
+                '{
+                    "request": "cashid:bitcoin.com/api/cashid?a=register&d=newsletter&r=i12l1c1&o=i458l3&x=95261230581",
+                    "address": "qqagsast3fq0g43wnrnweefjsk28pmyvwg7t0jqgg4"
+                }',
             ],
             [  // Mismatched domain
-                "{
-                    'request': 'cashid:bitcoin.com/api/cashid?a=register&d=newsletter&r=i12l1c1&o=i458l3&x=95261230581',
-                    'address': 'qqagsast3fq0g43wnrnweefjsk28pmyvwg7t0jqgg4',
-                    'signature': 'IKjtNWdIp+tofJQrhxBrq91jLwdmOVNlMhfnKRiaC2t2C7vqsHRoUA+BkdgjnOqX6hv4ZdeG9ZpB6dMh/sXJg/0='
-                }"
+                '{
+                    "request": "cashid:bitcoin.com/api/cashid?a=register&d=newsletter&r=i12l1c1&o=i458l3&x=95261230581",
+                    "address": "qqagsast3fq0g43wnrnweefjsk28pmyvwg7t0jqgg4",
+                    "signature": "IKjtNWdIp+tofJQrhxBrq91jLwdmOVNlMhfnKRiaC2t2C7vqsHRoUA+BkdgjnOqX6hv4ZdeG9ZpB6dMh/sXJg/0="
+                }'
             ],
         ];
     }
