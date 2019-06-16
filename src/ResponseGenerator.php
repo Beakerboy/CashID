eee4ac1459bf97cf5674a78b7d82e04b7df8b3bc
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-16 12:16:44 -0400

    Update ResponseGenerator.php

--- src/ResponseGenerator.php
+++ src/ResponseGenerator.php
@@ -2,6 +2,23 @@
 
 namespace CashID;
 
+use BitcoinPHP\BitcoinECDSA\BitcoinECDSA;
+
 class ResponseGenerator
 {
+    public function __construct($private_key, $metadata)
+    {
+        $this->bitcoinECDSA = new BitcoinECDSA();
+        $this->bitcoinECDSA->setPrivateKeyWithWif($private_key);
+        $this->metadata = $metadata;
+    }
+
+    public function createResponse($request_string): string
+    {
+    }
+    
+    private function signMessage(string $message): string
+    {
+        
+    }
 }
