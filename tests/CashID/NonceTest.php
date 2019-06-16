8c02878d53c19721e70ba15475648a168ef48eac
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-16 07:43:35 -0400

    Update NonceTest.php

--- tests/CashID/NonceTest.php
+++ tests/CashID/NonceTest.php
@@ -24,9 +24,9 @@ class NonceTest extends \PHPUnit\Framework\TestCase
     public function testRerunDuplicateNonce()
     {
         self::$randomValues = [100000000, 100000000];
-        $this->cashid = new CashID('me.com', '/api/parse.php');
-        $request1 = $this->cashid->createRequest();
-        $request2 = $this->cashid->createRequest();
+        $this->generator = new RequestGenerator('me.com', '/api/parse.php');
+        $request1 = $this->generator->createRequest();
+        $request2 = $this->generator->createRequest();
         $nonce1 = substr($request1, -9);
         $nonce2 = substr($request2, -9);
         
