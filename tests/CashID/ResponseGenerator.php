90898f6615c7c37942561c1cf9bb80038f957c70
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-17 07:35:28 -0400

    Update ResponseGenerator.php

--- tests/CashID/ResponseGenerator.php
+++ tests/CashID/ResponseGenerator.php
@@ -3,19 +3,36 @@
 namespace CashID\Tests\CashID;
 
 use BitcoinPHP\BitcoinECDSA\BitcoinECDSA;
+use CashID\API;
 
 class ResponseGenerator
 {
-    public function __construct($request, $metadata)
+    /**
+     * Create a new response generator
+     *
+     * @param array $metadata
+     *  all the metadata available to choose from
+     */
+    public function __construct($metadata)
     {
         $this->bitcoinECDSA = new BitcoinECDSA();
         $this->bitcoinECDSA->setPrivateKeyWithWif('L1M8W4jMqqu5h24Nzxf1sy5eHo2aSxdwab8h1fkP5Pt9ATfnxfda');
         $this->cashaddr = 'qpjvm3u8cvjddupctguwatrlaxtutprg8s04ekldyr';
-        $this->request = $request;
         $this->metadata = $metadata;
     }
 
-    public function createResponse($request_string): string
+    /**
+     * Create a response
+     *
+     * A JSON array is returned that includes all the required information
+     * in the CashID request. The $include_optional parameter determines
+     * if all or none of the optional fields are returned.
+     *
+     * @param array $request
+     * @param boolean $include_optional
+     * @returns string
+     */
+    public function createResponse($request_string, $include_optional = true): string
     {
         $converter = new \Submtd\CashaddrConverter\CashaddrConverter();
         
@@ -25,7 +42,16 @@ class ResponseGenerator
             "signature" => $this->signMessage($this->request),
         ];
     }
-    
+
+    /**
+     * Sign a message
+     *
+     * Use the object’s private key to sign the given message.
+     *
+     * @param string $message
+     * @param string
+     *  The signature string
+     */
     private function signMessage(string $message): string
     {
         return $this->bitcoinECDSA->signMessage($message, true);
