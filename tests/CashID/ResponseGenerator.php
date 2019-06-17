62dcfb11942275ef49eac79538e5baf75aced1e2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-16 20:35:32 -0400

    Update ResponseGenerator.php

--- tests/CashID/ResponseGenerator.php
+++ tests/CashID/ResponseGenerator.php
@@ -6,17 +6,18 @@ use BitcoinPHP\BitcoinECDSA\BitcoinECDSA;
 
 class ResponseGenerator
 {
-    public function __construct($private_key, $request)
+    public function __construct($request, $metadata)
     {
         $this->bitcoinECDSA = new BitcoinECDSA();
-        $this->bitcoinECDSA->setPrivateKeyWithWif($private_key);
+        $this->bitcoinECDSA->setPrivateKeyWithWif('L1M8W4jMqqu5h24Nzxf1sy5eHo2aSxdwab8h1fkP5Pt9ATfnxfda');
+        $this->cashaddr = 'qpjvm3u8cvjddupctguwatrlaxtutprg8s04ekldyr';
         $this->request = $request;
+        $this->metadata = $metadata;
     }
 
     public function createResponse($request_string): string
     {
         $converter = new \Submtd\CashaddrConverter\CashaddrConverter();
-        $cashaddr = $converter->convertToCashaddr($this->bitcoinECDSA->getWif());
         
         return [
             "request" => $this->request,
