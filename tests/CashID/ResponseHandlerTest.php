0245cca5d1fc92b2e6c09cc0ae40c6484a7c61d1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-19 15:09:14 -0400

    Update ResponseHandlerTest.php

--- tests/CashID/ResponseHandlerTest.php
+++ tests/CashID/ResponseHandlerTest.php
@@ -380,6 +380,40 @@ class ResponseHandlerTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
+     * Test that a response to an old request causes a failure
+     *
+     * @runInSeparateProcess
+     */
+    public function testOldRequest()
+    {
+        \CashID\TimeOverrider::setValues([strtotime('-1 month')]);
+        \CashID\TimeOverrider::setOverride();
+        $json_request = $this->generator->createRequest();
+        \CashID\TimeOverrider::unsetOverride();
+
+        // Create the response
+        $response_array = $this->response_generator->createResponse($json_request);
+        
+        // Validate against today's date and verify failure
+        $this->assertFalse($this->handler->validateRequest(json_encode($response_array)));
+
+        // Verify that the correct exception and message is produced
+        $this->expectOutputString("{'status': 142, 'message': 'The request has expired and is no longer available.'}");
+        $this->handler->confirmRequest();
+    }
+
+    /**
+     * Test that a response to a future request causes a failure
+     *
+     * @runInSeparateProcess
+     */
+    public function testFutureRequest()
+    {
+        // Generate an forward-dated request
+        // Validate against today's date
+    }
+
+    /**
      * @testCase ConfirmRequestHeadersSentException
      */
     public function testConfirmRequestHeadersSentException()
