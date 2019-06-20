01cdece351165cb150e1358e895be97d36277e1e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-20 09:29:18 -0400

    Create DefaultNotary.php

---
+++ src/DefaultNotary.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CashID;
+
+use BitcoinPHP\BitcoinECDSA\BitcoinECDSA;
+use Submtd\CashaddrConverter\CashaddrConverter;
+
+class DefaultNotary implements NotaryInterface
+{
+    public function checkSignature(string $address, string $signature, string $message)
+    {
+            $converter = new CashaddrConverter();
+            $legacy_address = $converter->convertFromCashaddr($address);
+            $bitcoinECDSA = new BitcoinECDSA();
+            return $bitcoinECDSA->checkSignatureForMessage($legacy_address, $signature, $message);
+    }
+
+    public function signMessage($address, $message)
+    {
+        $this->bitcoinECDSA = new BitcoinECDSA();
+        $converter = new CashaddrConverter();
+        $legacy_address = $converter->convertFromCashaddr($address);
+        $this->bitcoinECDSA->setPrivateKeyWithWif($legacy_address);
+        return $bitcoinECDSA->signMessage($message, true);
+    }
+}
