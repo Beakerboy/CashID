1fd1e2d8f8768f1aa7fbd3da11383539e2425f58
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-21 17:01:39 -0400

    Update RequestGeneratorTest.php

--- tests/CashID/RequestGeneratorTest.php
+++ tests/CashID/RequestGeneratorTest.php
@@ -57,26 +57,6 @@ class RequestGeneratorTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @testCase testRerunDuplicateNonce
-     * @runInSeparateProcess
-     */
-    public function testRerunDuplicateNonce()
-    {
-        $generator = new RequestGenerator("demo.cashid.info", "/api/parse.php");
-        $exp_nonce1 = 100000000;
-        $exp_nonce2 = 999999999;
-        $rand = $this->getFunctionMock("CashID", "rand");
-        $rand->expects($this->exactly(3))->will($this->onConsecutiveCalls($exp_nonce1, $exp_nonce1, $exp_nonce2));
-        $request1 = $generator->createRequest();
-        $request2 = $generator->createRequest();
-        $nonce1 = substr($request1, -9);
-        $nonce2 = substr($request2, -9);
-        
-        $this->assertEquals($exp_nonce1, $nonce1);
-        $this->assertEquals($exp_nonce2, $nonce2);
-    }
-
-    /**
      * @testCase testStorageFailure
      */
     public function testStorageFailure()
