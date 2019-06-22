6e689fed043774e316a1357cbfad4cab5ad6fbcd
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-22 18:40:07 -0500

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,4 @@
 <?php
-require_once 'Overriders/RandOverrider.php';
 require_once 'Overriders/TimeOverrider.php';
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     require_once(__DIR__ . '/../vendor/autoload.php');
