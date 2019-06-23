f9fffc5fb8cd6c490fe8e2bfb090c061bee638d9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-22 20:18:51 -0500

    Update RequestGeneratorTest.php

--- tests/CashID/RequestGeneratorTest.php
+++ tests/CashID/RequestGeneratorTest.php
@@ -56,6 +56,24 @@ class RequestGeneratorTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
+     * @testCase testRerunDuplicateNonce
+     */
+    public function testRerunDuplicateNonce()
+    {
+        $generator = new RequestGenerator("demo.cashid.info", "/api/parse.php");
+        RandOverrider::setOverride();
+        RandOverrider::setValues([100000000, 100000000, 100000001]);
+        $request1 = $generator->createRequest();
+        $request2 = $generator->createRequest();
+        RandOverrider::unsetOverride();
+        $nonce1 = substr($request1, -9);
+        $nonce2 = substr($request2, -9);
+        
+        $this->assertEquals(100000000, $nonce1);
+        $this->assertEquals(100000001, $nonce2);
+    }
+
+    /**
      * @testCase testStorageFailure
      */
     public function testStorageFailure()
