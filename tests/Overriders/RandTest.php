c8a3da8c67752ad9f440342189d94081ef64103e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-24 18:09:46 -0500

    Update RandTest.php

--- tests/Overriders/RandTest.php
+++ tests/Overriders/RandTest.php
@@ -11,17 +11,9 @@ class RandTest extends \PHPUnit\Framework\TestCase
      */
     public function testRerunDuplicateNonce()
     {
-        $generator = new RequestGenerator("demo.cashid.info", "/api/parse.php");
-        $exp_nonce1 = 100000000;
-        $exp_nonce2 = 999999999;
         $rand = $this->getFunctionMock(__NAMESPACE__, "rand");
         $rand->expects($this->twice())->willReturn(100000000, 100000001);
-        $request1 = $generator->createRequest();
-        $request2 = $generator->createRequest();
-        $nonce1 = substr($request1, -9);
-        $nonce2 = substr($request2, -9);
-        
-        $this->assertEquals($exp_nonce1, $nonce1);
-        $this->assertEquals($exp_nonce2, $nonce2);
+        $this->assertEquals(100000000, rand());
+        $this->assertEquals(100000001, rand(();
     }
 }
