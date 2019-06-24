2b3161972d90243a148f596d46bfab97f84f6a16
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-24 16:43:20 -0500

    Update RandTest.php

--- tests/Overriders/RandTest.php
+++ tests/Overriders/RandTest.php
@@ -7,6 +7,10 @@ use CashID\RequestGenerator;
 class RandTest extends \PHPUnit\Framework\TestCase
 {
     use \phpmock\phpunit\PHPMock;
+    function setup()
+    {
+        $this->return = 100000000;
+    }
 
     /**
      * @testCase testRerunDuplicateNonce
@@ -18,8 +22,9 @@ class RandTest extends \PHPUnit\Framework\TestCase
         $exp_nonce1 = 100000000;
         $exp_nonce2 = 999999999;
         $rand = $this->getFunctionMock("CashID", "rand");
-        $rand->expects($this->exactly(6))->will($this->onConsecutiveCalls(100000000, 100000001, 100000002, 100000003, 100000004, 100000005, 999999999));
+        $rand->expects($this->exactly(6))->willReturn($this->return);
         $request1 = $generator->createRequest();
+        $this->return++;
         $request2 = $generator->createRequest();
         $nonce1 = substr($request1, -9);
         $nonce2 = substr($request2, -9);
