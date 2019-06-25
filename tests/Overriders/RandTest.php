2792468017f29e65a8391cf8a4a3b2dff22a4020
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-24 20:15:14 -0500

    Update RandTest.php

--- tests/Overriders/RandTest.php
+++ tests/Overriders/RandTest.php
@@ -11,7 +11,7 @@ class RandTest extends \PHPUnit\Framework\TestCase
      */
     public function testRerunDuplicateNonce()
     {
-        \phpmock\phpunit\PHPMock::defineFunctionMock();
+        \phpmock\phpunit\PHPMock::defineFunctionMock("CashID", "rand");
         $rand = $this->getFunctionMock(__NAMESPACE__, "rand");
         $rand->expects($this->twice())->willReturn(100000000, 100000001);
         $this->assertEquals(100000000, rand());
