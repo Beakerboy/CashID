3c607d9a170d62a6fa4449e6e578d8fac37a114f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-24 22:58:35 -0500

    Update RandTest.php

--- tests/Overriders/RandTest.php
+++ tests/Overriders/RandTest.php
@@ -13,7 +13,7 @@ class RandTest extends \PHPUnit\Framework\TestCase
     {
         \phpmock\phpunit\PHPMock::defineFunctionMock("CashID", "rand");
         $rand = $this->getFunctionMock(__NAMESPACE__, "rand");
-        $rand->expects($this->twice())->willReturn(100000000, 100000001);
+        $rand->expects($this->any())->willReturn(100000000, 100000001);
         $this->assertEquals(100000000, rand());
         $this->assertEquals(100000001, rand());
     }
