4b8077d73e09dd56019cbc5093f91546cfee9433
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-06-26 06:56:57 -0500

    Update RandTest.php

--- tests/Overriders/RandTest.php
+++ tests/Overriders/RandTest.php
@@ -9,17 +9,19 @@ class RandTest extends \PHPUnit\Framework\TestCase
      */
     public function testRerunDuplicateNonce()
     {
+        $exp_nonce1 = rand(100000000, 999999999);
+        $exp_nonce2 = rand(100000000, 999999999);
         \CoreOverrider\OverriderBase::createMock("CashID", "rand");
         $generator = new RequestGenerator("demo.cashid.info", "/api/parse.php");
         RandOverrider::setOverride();
-        RandOverrider::setValues([100000000, 100000000, 100000001]);
+        RandOverrider::setValues([$exp_nonce1, $exp_nonce1, $exp_nonce2]);
         $request1 = $generator->createRequest();
         $request2 = $generator->createRequest();
         RandOverrider::unsetOverride();
         $nonce1 = substr($request1, -9);
         $nonce2 = substr($request2, -9);
         
-        $this->assertEquals(100000000, $nonce1);
-        $this->assertEquals(100000001, $nonce2);
+        $this->assertEquals($exp_nonce1, $nonce1);
+        $this->assertEquals($exp_nonce2, $nonce2);
     }
 }
