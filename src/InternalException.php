4d7af76fb6443b2035f1b96a49415d8d8b5ad40a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-07-23 06:34:56 -0400

    Phpcs ws

--- src/InternalException.php
+++ src/InternalException.php
@@ -1,7 +1,7 @@
 <?php
 namespace CashID;
 
-// Create an internal exception type which lets us catch our own exceptions 
+// Create an internal exception type which lets us catch our own exceptions
 // while still passing on system exceptions that we didn't handle.
 class InternalException extends \Exception
 {
