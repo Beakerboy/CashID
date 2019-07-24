fc505c980bd1808d7a2e3414a5c1b56eb108e99f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-07-24 08:32:41 -0400

    Documentation
    #10

--- src/Cache/RequestCacheInterface.php
+++ src/Cache/RequestCacheInterface.php
@@ -2,9 +2,40 @@
 
 namespace CashID\Cache;
 
+/**
+ * Request Cache Interface
+ *
+ * An interface that defines the means of interacting with a key value cache.
+ * The cache is used to store CashID requests that have been send by the
+  * server, with the nonce as the unique key.
+ */
 interface RequestCacheInterface
 {
+    /**
+     * Store
+     *
+     * Store a key / value pair in the cache
+     *
+     * @param string $key
+     * @param mixed $var
+     */
     public function store(string $key, $var);
+
+    /**
+     * Fetch
+     *
+     * Fetch the value of a key from the cache
+     *
+     * @param string $key
+     */
     public function fetch(string $key);
+
+    /**
+     * Exists
+     *
+     * Check if a key exists in the cache
+     *
+     * @param string $key
+     */
     public function exists(string $key);
 }
