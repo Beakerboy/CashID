749cd21dfd31a708f8fc58582bc18884ea5f6b56
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-07-25 07:11:25 -0400

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,4 @@
 <?php
-require_once 'Overriders/OverriderBase.php';
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     require_once(__DIR__ . '/../vendor/autoload.php');
 } elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
