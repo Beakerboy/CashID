aaec5b6f803075fb60fc138453fd4b50eb995703
Kevin Nowaczyk <beakerboy99@yahoo.com>
2019-07-26 08:39:59 -0400

    Create CashIDService.php

---
+++ src/Services/CashIDService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace CashID/Services;
+
+abstract class CashIDService
+{
+    protected $service_domain;
+    protected $service_path;
+    protected $cache;
+    protected $defaultDependencies;
+
+    public function __construct(string $domain, string $path, ...$dependencies)
+    {
+        $this->service_domain = $domain;
+        $this->service_path = $path;
+        $this->setDependencies($dependencies);
+    }
+
+    /**
+     * Set the class dependencies
+     *
+     * @param array $dependencies
+     *   an array of objects
+     */
+    private function setDependencies(array $dependencies)
+    {
+        // Assign the provided dependency to the property
+        foreach ($dependencies as $object) {
+            $interface_names = class_implements($object);
+            $used = false;
+            // Check the list of interfaces to ensure it implements one we need
+            foreach ($interface_names as $interface_name) {
+                // If it does, save it to the appropriate class parameter
+                if (isset($this->defaultDependencies[$interface_name])) {
+                    $name = $this->defaultDependencies[$interface_name]['name'];
+                    $this->$name = $object;
+                    unset($this->defaultDependencies[$interface_name]);
+                    $used = true;
+                }
+            }
+            // If it does not, throw an exception
+            if (!$used) {
+                $class_name = get_class($object);
+                throw new \Exception("Class '{$class_name}' cannot be used as a dependency.");
+            }
+        }
+        foreach ($this->defaultDependencies as $dependency) {
+            $name = $dependency['name'];
+            $class = $dependency['class'];
+            $this->$name = new $class;
+        }
+    }
+}
